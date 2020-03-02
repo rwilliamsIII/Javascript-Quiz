a0e7be463cf3a6650684e67e678ec4ebcb22ef49
Robert Williams <robert@a3marketing.com>
2020-03-01 20:24:16 -0600

    started code to generate first question and answers

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 var startButton = document.getElementById("start-btn")
 var questionBox = document.getElementById("question-box")
+var questionElement = document.getElementById("question")
+var answerButtonsElement = document.getElementById("answer-buttons")
+let shuffledQuestions, currentQuestionIndex
+var questions = [
+    {
+        question: "What is Javascript?",
+        answers: [
+            {text: "A recipe for coffee", correct: false},
+            {text: "A worse version of JQuery", correct: false},
+            {text: "An object-oriented computer programming language commonly used to create interactive effects within web browsers.", correct: true},
+            {text: "A common torture practice", correct: false}
+        ]
+    }
+]
 startButton.addEventListener("click", startGame)
 
 
@@ -8,11 +22,19 @@ startButton.addEventListener("click", startGame)
 function startGame() {
     console.log("Started Game")
     startButton.classList.add("hide")
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
     questionBox.classList.remove("hide")
+    nextQuestion()
 }
 
 function nextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
 
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    answerButtonsElement.innerText = question.answers
 }
 
 function selectAnswer() {
