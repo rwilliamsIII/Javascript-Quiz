a643418eb5bca1584ca8ba788bfc6f53a466c78b
Robert Williams <robert@a3marketing.com>
2020-03-01 21:36:32 -0600

    added header for timer interval

--- script.js
+++ script.js
@@ -3,17 +3,74 @@ var questionBox = document.getElementById("question-box")
 var questionElement = document.getElementById("question")
 var answerButtonsElement = document.getElementById("answer-buttons")
 let shuffledQuestions, currentQuestionIndex
+var secondsLeft = 10;
+
 var questions = [
     {
-        question: "What is Javascript?",
-        answers: [
-            {text: "A recipe for coffee", correct: false},
-            {text: "A worse version of JQuery", correct: false},
-            {text: "An object-oriented computer programming language commonly used to create interactive effects within web browsers.", correct: true},
-            {text: "A common torture practice", correct: false}
-        ]
+        question: 'What is Javascript?',
+        potential_answers: [
+            {
+                id: 'a',
+                answer: 'An object-oriented computer programming language commonly used to create interactive effects within web browsers.'
+            },
+            {
+                id: 'b',
+                answer: 'A recipe for coffee.'
+            },
+            {
+                id: 'c',
+                answer: 'A worse version of JQuery.'
+            },
+            {
+                id: 'd',
+                answer: "A common torture practice."
+            }
+        ],
+        correct_answer: 'a'
+    },
+    {
+        question: 'Inside which HTML element do we put the JavaScript?',
+        potential_answers: [
+            {
+                id: 'a',
+                answer: '<javascript>'
+            },
+            {
+                id: 'b',
+                answer: '<script>'
+            },
+            {
+                id: 'c',
+                answer: '<scripting>'
+            },
+            {
+                id: 'd',
+                answer: '<js>'
+            }
+        ],
+        correct_answer: 'b'
+    },
+    {
+        question: 'Where is the correct place to insert a JavaScript tag?',
+        potential_answers: [
+            {
+                id: 'a',
+                answer: 'In the <head> section'
+            },
+            {
+                id: 'b',
+                answer: 'Where ever you want. JS is the wild west, no rules!'
+            },
+            {
+                id: 'c',
+                answer: 'At the end of the <body> section'
+            }
+            
+        ],
+        correct_answer: 'c'
     }
 ]
+
 startButton.addEventListener("click", startGame)
 
 
@@ -21,10 +78,10 @@ startButton.addEventListener("click", startGame)
 
 function startGame() {
     console.log("Started Game")
-    startButton.classList.add("hide")
+    
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
-    questionBox.classList.remove("hide")
+    
     nextQuestion()
 }
 
@@ -34,7 +91,7 @@ function nextQuestion() {
 
 function showQuestion(question) {
     questionElement.innerText = question.question
-    answerButtonsElement.innerText = question.answers
+    answerButtonsElement.innerText = questions.answers
 }
 
 function selectAnswer() {
