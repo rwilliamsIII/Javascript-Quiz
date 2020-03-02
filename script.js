ea32d14d13adb2e77713cc5e8617e0968325d5f0
Robert Williams <robert@a3marketing.com>
2020-03-01 19:22:03 -0600

    created 3 primary functions for quiz and added event listener to start button

---
+++ script.js
@@ -0,0 +1,18 @@
+var startButton = document.getElementById("start-btn")
+
+startButton.addEventListener("click", startGame)
+
+
+
+
+function startGame() {
+    console.log("Started Game")
+}
+
+function nextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
