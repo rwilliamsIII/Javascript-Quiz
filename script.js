8b46db880f3c77d48ea404dbdb58950fedebfb64
Robert Williams <robert@a3marketing.com>
2020-03-01 19:26:16 -0600

    created function to start game and pull up first question

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var startButton = document.getElementById("start-btn")
-
+var questionBox = document.getElementById("question-box")
 startButton.addEventListener("click", startGame)
 
 
@@ -7,6 +7,8 @@ startButton.addEventListener("click", startGame)
 
 function startGame() {
     console.log("Started Game")
+    startButton.classList.add("hide")
+    questionBox.classList.remove("hide")
 }
 
 function nextQuestion() {
